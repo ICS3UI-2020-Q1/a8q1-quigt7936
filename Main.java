646bb69801611a558e3d44df2f0f61e6543a63d4
Thomas Quigley <quigt7936@wrdsb.ca>
2020-10-30 14:56:33 +0000

    Completed question

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+/*
+ * This program asks for a name then says hello to the user
+ * @author Thomas Quigley
+*/
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -5,21 +10,50 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
+  JPanel mainPanel;
+  JTextField inputName;
+  JTextField outputGreeting;
+  JButton sayHello;
+
   
 
 
   // Method to assemble our GUI
   public void run(){
     // Creats a JFrame that is 800 pixels by 600 pixels, and closes when you click on the X
-    JFrame frame = new JFrame("Title");
+    JFrame frame = new JFrame("Hello!");
     // Makes the X button close the program
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     // makes the windows 800 pixel wide by 600 pixels tall
     frame.setSize(800,600);
     // shows the window
     frame.setVisible(true);
- 
-    
+
+    // initialize the main panel and add it to the frame
+    mainPanel = new JPanel();
+    mainPanel.setLayout(null);
+    frame.add(mainPanel);
+
+    // make the text fields
+    inputName = new JTextField();
+    inputName.setBounds(200, 100, 400, 30);
+
+    outputGreeting = new JTextField();
+    outputGreeting.setBounds(200, 300, 400, 30);
+    outputGreeting.setEnabled(false);
+
+    // make the button
+    sayHello = new JButton("Say Hello");
+    sayHello.setBounds(350, 450, 100, 30);
+    sayHello.addActionListener(this);
+    sayHello.setActionCommand("sayHello");
+
+
+    // add the fields and button to the mainPanel
+    mainPanel.add(inputName);
+    mainPanel.add(outputGreeting);
+    mainPanel.add(sayHello);
+  
 
   }
 
@@ -27,6 +61,13 @@ public class Main implements Runnable, ActionListener{
   public void actionPerformed(ActionEvent e){
     // get the command from the action
     String command = e.getActionCommand();
+
+    if (command.equals("sayHello")) {
+      // button was pressed
+      // say hello
+      String name = inputName.getText();
+      outputGreeting.setText("Hello " + name);
+    }
 
   }
 
